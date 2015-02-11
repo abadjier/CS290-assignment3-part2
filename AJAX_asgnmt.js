7242c420cfd4ebe68cd23cd8491479bc3d65696e
Ralitza_A <abadjier@onid.oregonstate.edu>
2015-02-11 15:46:08 +0200

    Add to local storage functionality

--- AJAX_asgnmt.js
+++ AJAX_asgnmt.js
@@ -1,9 +1,17 @@
+var stored_gists = null;
 
-function generate_rslt() {
- 
+
+function generate_rslt() { 
+  var res_list = document.getElementById("res");
+	
+  // 	Clear the old result list
+	while( res_list.childNodes.length != 0 ) {
+	  res_list.removeChild(res_list.childNodes[res_list.childNodes.length - 1]);
+	}
+	
+  // generate new result list	
 	getGists();
-
-};
+}
 
 function getSelectedLanguages() {
   var lang_select = [];
@@ -25,22 +33,66 @@ function getSelectedLanguages() {
 	};
 	
 	return lang_select;
-};
+}
 
 function createResultsList(ul, description, url, language) {
+
+	var item = '<a href="' + url + '">' + description + '</a>';
+
+	// check if selected result is already in favorites list
+	if (isInFavorites(item)) {
+		return;
+	}
+
   var res_item = document.createElement('li');
-	res_item.innerHTML = '<a href="' + url + '">' + description + '</a>';
-	ul.appendChild(res_item);
-};
+	var fav_btn = document.createElement('button');
+	var btn_text = document.createTextNode('Add to Favorites');
+	fav_btn.appendChild(btn_text);
+	res_item.innerHTML = item; 
 
+	res_item.appendChild(fav_btn);	
+	ul.appendChild(res_item);	
+}
 
+function isInFavorites(item) {
+	var i = 0;
+  var gist;
+ 
+  for (i = 0; i < stored_gists.gists.length; i++) {
+	
+		gist = stored_gists.gists[i];
+		
+		if (gist == item) {
+			return true;
+		}
+  }
+	
+  return false;
+}
+
+function addToFavoritesList(ul, itemText) {
+  var fav_item = document.createElement('li');
+	fav_item.innerHTML = itemText;
+	ul.appendChild(fav_item);	
+}
+
+function populateFavoritesList( ul, fav_gists ) {  
+	fav_gists.gists.forEach( function (gist) { 
+	  var liItem = document.createElement('li');
+		var btn = document.createElement('button');
+	  var btn_text = document.createTextNode('Remove');
+	  btn.appendChild(btn_text);
+		liItem.innerHTML = gist;	
+		liItem.appendChild(btn);
+		ul.appendChild(liItem);
+	});
+}
 
 function getGists() {
 
   var pages = document.getElementById('num_pgs');
 	var url;	
-  var req;
-	
+  var req;	
 	
 	// source: http://stackoverflow.com/questions/1085801/get-selected-value-of-dropdownlist-using-javascript
 	var num_of_pgs = pages.options[pages.selectedIndex].value; 
@@ -58,8 +110,7 @@ function getGists() {
 			var files;
 			var file;
 			
-			if(this.readyState ===4){
-		    console.log('---------------********************');
+			if(this.readyState ===4){		    
 			  result = JSON.parse(this.responseText);		  
         //source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
 				Array.prototype.push.apply(gists, result);
@@ -85,27 +136,106 @@ function getGists() {
 					  for (var j = 0; j < language.length; j++) {
 					    select_lang.forEach( function (lang) {
 						    if (language[j] === lang) {
-							    createResultsList(document.getElementById('results'), description, url, language);
+							    createResultsList(document.getElementById('res'), description, url, language);
 							  }
 						  });
 					  }
 				  }
 					else {
 					  // selected language array is empty
-						createResultsList(document.getElementById('results'), description, url, language);
+						createResultsList(document.getElementById('res'), description, url, language);
 					}
           
 				});
 			}
-	  }
+	  }		
 		
-		
-		url = 'https://api.github.com/gists?page=' + i + '&per_page=30';
-			
+		url = 'https://api.github.com/gists?page=' + i + '&per_page=30';			
 		
 		req.open('GET', url);
 	  req.send();
+	}	
+}
+
+function setUpResultsList() {
+  //Hook up event listeners to results buttons
+  ul = document.getElementById('res');
+
+  // Code for add to favourites button
+  //	Source: Jon Duckett 'Javascript & Jquery' pg. 269  
+	function getTarget(e) {
+    if (!e) {         // If there is no event object 
+		  e = window.event;   // Use old IE event object
+		}
+		return e.target || e.srcElement;   // get the target of event
+  }	
+	
+	function addToFavorites (e) {
+    var target, liElement, ulElement, link;	
+
+    if (e.target.nodeName !== 'BUTTON') {
+      return;		
+		}
+		
+    target = getTarget(e);           // get clicked on button
+		
+		liElement = target.parentNode;		// get li element
+		ulElement = target.parentNode.parentNode; //get ul element		
+				
+		link = liElement.innerHTML;		
+		link = link.slice(0, link.indexOf("<button>"));   // remove button from li
+		//console.log(link);
+		
+		// Add link to favourites on screen and to local storage ------TO DO
+		//Add to local storage
+		stored_gists.gists.push(link);
+		localStorage.setItem('storedGistsKey', JSON.stringify(stored_gists));
+		
+		//Add to favourites
+		addToFavoritesList(document.getElementById('favorites'), link);
+					
+		ulElement.removeChild(liElement);				
+  }	
+	
+	if (ul.addEventListener) { //If event listener works
+	  ul.addEventListener('click', function(e) {     // Add listener on click
+	    addToFavorites(e);
+		}, false);               // use bubbling phase for flow
+	}
+	else {
+	  ul.attachEvent('onclick', function(e) {     // Use old IE model
+		  addToFavorites(e);
+		});
 	}
 
+}
+
+function setUpFavoritesList() {
+
+}
+
+window.onload = function () {
+
+  // Load favourites from local storage --------TO DO
+	
+	
+	// Initialize storage
+	var gistStr = localStorage.getItem('storedGistsKey');
+	
+	if (gistStr === null) {
+	  stored_gists = {'gists':[]};
+		// convert into string representation of an object
+		localStorage.setItem('storedGistsKey', JSON.stringify(stored_gists));  
+	}
+	else {
+	  stored_gists = JSON.parse(gistStr);
+	}
+	
+	populateFavoritesList( document.getElementById('favorites'), stored_gists );
+	//console.log('stored: ' + stored_gists);
+	
+	setUpResultsList();
+	
+	setUpFavoritesList();
 	
 }
